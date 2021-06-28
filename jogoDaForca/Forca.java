1af7b6779c6daa3ce2b26790b6e75a6d06b24a5a
tuliofh01 <tuliofh01@gmail.com>
2021-06-27 21:59:24 -0300

    Jogo da forca simples em java

---
+++ jogoDaForca/Forca.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Forca  {
+    public static void main(String[] args) {
+        Janela janela = new Janela();
+        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
